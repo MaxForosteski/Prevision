8269c381dbb8ee26cebc89ab0383845c690cc40c
Max Forosteski <maxforosteski12@gmail.com>
2023-05-29 10:56:04 -0300

    Fundo e Nav

--- Script.js
+++ Script.js
@@ -1,5 +1,11 @@
 const containerScroll = document.querySelector('.container-scroll');
-
+    const scrollWidth = containerScroll.scrollHeight
+    const containerWidth = containerScroll.offsetWidth;
 setInterval(() => {
-    containerScroll.scrollLeft +=2;
+    containerScroll.scrollLeft +=2
+    if(scrollPositiion >= (scrollWidth - containerWidth)){
+       
+        scrollPosition = 0
+    
+    }
 },20);
